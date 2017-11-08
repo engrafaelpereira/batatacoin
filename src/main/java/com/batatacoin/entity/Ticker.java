269ee1b93a8303201f4698f41fd9f82c978c2d9e
Evandro Junqueira Ramos <eramos@cit>
2017-11-08 19:31:15 -0200

    Ticker entity

---
+++ src/main/java/com/batatacoin/entity/Ticker.java
@@ -0,0 +1,80 @@
+package com.batatacoin.entity;
+
+import java.math.BigDecimal;
+
+public class Ticker {
+
+	public BigDecimal High;
+	
+	public BigDecimal Low;
+	
+	public BigDecimal Vol;
+	
+	public BigDecimal Last;
+
+	public BigDecimal Sell;
+	
+	public BigDecimal Date;
+	
+	public BigDecimal Buy;
+
+	public BigDecimal getHigh() {
+		return High;
+	}
+
+	public void setHigh(BigDecimal high) {
+		High = high;
+	}
+
+	public BigDecimal getLow() {
+		return Low;
+	}
+
+	public void setLow(BigDecimal low) {
+		Low = low;
+	}
+
+	public BigDecimal getVol() {
+		return Vol;
+	}
+
+	public void setVol(BigDecimal vol) {
+		Vol = vol;
+	}
+
+	public BigDecimal getLast() {
+		return Last;
+	}
+
+	public void setLast(BigDecimal last) {
+		Last = last;
+	}
+
+	public BigDecimal getSell() {
+		return Sell;
+	}
+
+	public void setSell(BigDecimal sell) {
+		Sell = sell;
+	}
+
+	public BigDecimal getDate() {
+		return Date;
+	}
+
+	public void setDate(BigDecimal date) {
+		Date = date;
+	}
+
+	public BigDecimal getBuy() {
+		return Buy;
+	}
+
+	public void setBuy(BigDecimal buy) {
+		Buy = buy;
+	}
+	
+	
+	
+	
+}
